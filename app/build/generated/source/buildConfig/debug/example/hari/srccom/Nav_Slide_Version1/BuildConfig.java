d7386d0288f052d5fbd9d3badcda894856b72ea1
Hari Pancha <32916178+hpancha87@users.noreply.github.com>
2017-11-05 14:57:00 -0500

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/example/hari/srccom/Nav_Slide_Version1/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package example.hari.srccom.Nav_Slide_Version1;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "example.hari.srccom.Nav_Slide_Version1";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
